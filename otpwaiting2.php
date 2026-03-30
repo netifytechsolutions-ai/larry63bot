03c72c3788631dfed23be65d0a414e67c978f44c
dennis <netifytechsolutions@gmail.com>
2026-03-30 21:53:45 +0300

    fix

--- otpwaiting2.php
+++ otpwaiting2.php
@@ -14,7 +14,7 @@ $statuses = file_exists($file) ? json_decode(file_get_contents($file), true) : [
 $status = $statuses[$phone]['otp'] ?? '';
 
 if($status === 'approve'){
-    header("Location: otp5.php?phone=$phone");
+    header("Location: success.php?phone=$phone");
     exit();
 }
 
